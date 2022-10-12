c45f9cd8c754c77001010b03a6d00d8bd7b6ecfc
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2022-10-13 00:07:43 +0330

    Initial Entry

---
+++ Append_to_List.py
@@ -0,0 +1,14 @@
+ledger = []
+
+def AppendToList(name="", age=0):
+  thisdict  = {}
+  thisdict["Name"] = name
+  thisdict["Age"] = age
+  ledger.append(thisdict)
+  #print(thisdict)
+
+AppendToList("Ali", 20)
+print(ledger)
+
+AppendToList("Reza", 32)
+print(ledger)
---
+++ Visual Studio Code Extensions.md
@@ -0,0 +1,8 @@
+It's about using the **Visual Studio Code**.
+
+Necessary Extensions for VS Code:
+- Python
+- Code Runner
+
+Optional Extensions for VS Code:
+- Material Icon Theme
