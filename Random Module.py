4f9163dfa017f2229c7d376856ac7aef35ce1e19
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2022-10-13 22:40:20 +0330

    Initial Entry

---
+++ Random Module.py
@@ -0,0 +1,15 @@
+# GitHub.com/AliRezaJoodi
+
+import random
+
+number = random.randrange(5,10)
+print("Random Number (5 to 10): ", number)
+
+number = random.randrange(10)
+print("Random Number (0 to 10): ", number)
+
+number = random.randint(5,10)
+print("Random Number (5 to 10): ", number)
+
+name = ['Ali', 'Bob','Daniel', 'Hoouk', 'Starwalker']
+print("Random Name: ", random.choice(name))
