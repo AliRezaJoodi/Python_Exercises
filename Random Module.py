9be06bcd58f53ec3db5e68c54a3ee6ec1467400a
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2022-10-15 00:06:14 +0330

    Update

--- Random Module.py
+++ Random Module.py
@@ -3,16 +3,22 @@
 import random
 
 number = random.randrange(5,10)
-print("Random Number (5 to 10): ", number)
+print("randrange (5 to 10):", number)
 
 number = random.randrange(10)
-print("Random Number (0 to 10): ", number)
+print("randrange (10):", number)
 
 number = random.randint(5,10)
-print("Random Number (5 to 10): ", number)
+print("randint (5 to 10):", number)
 
 name = ['Ali', 'Bob','Daniel', 'Hoouk', 'Starwalker']
-print("Random Name: ", random.choice(name))
+txt_str = random.choice(name)
+print("random.choice:", txt_str)
+#print(type(txt_str))
 
-txt="!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-print("Password Generator: ", random.sample(txt,8))
+txt_list = random.choices(name)
+print("random.choices:", txt_list)
+#print(type(txt_list))
+
+txt="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+print("random.sample:", random.sample(txt,8))
